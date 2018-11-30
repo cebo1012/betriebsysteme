677c67d6a3ef7185102f359ef9112e3c3226e93a
cebo1012 <38214923+cebo1012@users.noreply.github.com>
2018-11-30 22:23:08 +0100

    Update root.cpp

--- src/root.cpp
+++ src/root.cpp
@@ -1,43 +1,39 @@
-// TODO LinkedList, bibliotheken anschauen
-/*void MyRoot::addFile(string name, off_t size, mode_t mode) {
-	myFile copyfile[sizeof(files) + 1];
-	int z = 0;
-	for (int k = 0; k < (sizeof(files) + 1); k++, z++) {
-		copyfile[z] = files[k];
-	}
-	//Speichern von Name, Dateigroesse und Zugriffsrechte pro Datei
-	myFile f = new myFile(name, size, mode);
-	copyfile[i] = f;
-	files = copyfile.clone();
+// TODO LinkedList, bibliotheken anschauen std::list
+#include <list>
+myRoot::myRoot(myFile firstfile) {
+	file.push_front(firstfile);
+}
+myRoot::myRoot(string name, off_t size, mode_t mode) {
+	myFile firstfile = new myFile(name, size, mode);
+	file.push_front(firstfile);
+}
+myRoot::~myRoot() {
+	files.clear();
 }
 
-int MyRoot::deleteFile(string name) {
-	myFile filetodelete = getFile(name);
+void myRoot::addFile(string name, off_t size, mode_t mode) {
+	//Speichern von Name, Dateigroesse und Zugriffsrechte pro Datei
+	myFile f = new myFile(name, size, mode);
+	files.push_back(f);
+}
+
+void myRoot::deleteFile(string name) {
 	//In Root Verzeichnis Datei loeschen
-	int i = 0;
-	while (filetodelete.name != files[i].name && filetodelete.size != files[i].size && filetodelete.mode != files[i].mode) {
-		i++;
-	}
-	//Nachfolgende Elemente um eine Stelle nach hinten kopieren und ueberschreiben und
-	//dabei in ein anderes Array kopieren um anschlie�end die Laenge von Files zu verkuerzen
-	int indexfiles = i;
-	myFile array[sizeof(files) - 1];
-	for (int indexarray = 0; index < sizeof(files - 1) && indexarray < sizeof(array - 1); index++, indexarray++) {
-		files[index] = files[index + 1];
-		array[indexarray] = files[indexarray];
-	}
-	//Laenge von Files um eins verkuerzen --- Falls Blockanzahl nicht konstant
-	//std::array<myFile,> a = { 1,2,3 };	
-return 0;
+	if(files.size() != 0){
+	myFile filetodelete = files.getFile(name);
+	files.erase(filetodelete);}
+
 }
 myFile ROOT::getFile(string name) {
-	int i = 0;
-	while (!(strcmp(name, files[i].name))) {
-		i++;
+	it = files.begin();
+	while (it != files.end() || *it.name != name)
+	{
+		it++;
 	}
-	return files[i];
+	return *it;
 }
-*/
+
+
 
 //Tanja add: i need it to have a list from all files
 MyFile MyRoot::getFile(int n) {
